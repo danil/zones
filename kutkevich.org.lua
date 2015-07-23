bba621590556a15fab1f72f79ac101a7df0ce9f3
Danil Kutkevich <danil@kutkevich.org>
2015-07-23 17:43:40 +0000

    kutkevich.org: add SRV records to lua

--- kutkevich.org.lua
+++ kutkevich.org.lua
@@ -5,6 +5,9 @@
 -- _a = "kutkevich.org"
 
 -- A records
+-- @name  = relative name
+-- @ip    = IPv4 address
+-- @ttl   = TTL (default: user default TTL)
 a("lisa", "188.134.8.88")
 a("minecraft", "23.235.235.55")
 a("tatyana", "188.134.8.88")
@@ -12,6 +15,10 @@ a("yurij", "188.134.2.138")
 a(_a, "178.79.163.10")
 
 -- MX records
+-- @name      = relative name
+-- @exchanger = mail exchanger(fqdn)
+-- @prio      = priority (default: 0)
+-- @ttl       = TTL (default: user default TTL)
 mx(_a, "in1-smtp.messagingengine.com", 10)
 mx(_a, "in2-smtp.messagingengine.com", 20)
 mx(concat("g", _a), "alt1.aspmx.l.google.com", 5)
@@ -21,6 +28,9 @@ mx(concat("g", _a), "aspmx2.googlemail.com", 10)
 mx(concat("g", _a), "aspmx3.googlemail.com", 10)
 
 -- CNAME records
+-- @name    = relative name
+-- @alias   = alias (fqdn)
+-- @ttl     = TTL (default: user default TTL)
 cname("danil", _a)
 cname("elpa", _a)
 cname("ftp", _a)
@@ -37,3 +47,15 @@ cname("static", _a)
 cname("sync", _a)
 cname("syncthing", _a)
 cname("www", _a)
+
+-- SRV records
+-- @name    = relative name
+-- @target  = host name(fqdn)
+-- @port    = port number
+-- @prio    = prio (default: 0)
+-- @weight  = weight (default: 0)
+-- @ttl     = TTL (default: user default TTL)
+-- srv(name, target, port, prio, weight, ttl)
+srv("_xmpp-client._tcp", "chat.messagingengine.com", 5222, 5)
+srv("_xmpp-server._tcp", "chat.messagingengine.com", 5269, 5)
+srv("_xmpp._tcp", "chat.messagingengine.com", 5269, 5)
