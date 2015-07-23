104054fd70e3b4f7bcd7a3fbb021d4e923bb98b9
Danil Kutkevich <danil@kutkevich.org>
2015-07-23 14:19:04 +0000

    NEW: kutkevich.org lua zone file

---
+++ kutkevich.org.lua
@@ -0,0 +1,29 @@
+-- File: kutkevich.org.lua
+-- Zone: kutkevich.org
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "kutkevich.org"
+
+-- A records
+a("lisa", "188.134.8.88")
+a("minecraft", "23.235.235.55")
+a("tatyana", "188.134.8.88")
+a("yurij", "188.134.2.138")
+a(_a, "178.79.163.10")
+
+-- CNAME records
+cname("danil", _a)
+cname("elpa", _a)
+cname("ftp", _a)
+cname("gentoo", _a)
+cname("git", _a)
+cname("h2", _a)
+cname("h5a", _a)
+cname("munin.h2", _a)
+cname("ntp", _a)
+cname("proxy", _a)
+cname("staging", _a)
+cname("static", _a)
+cname("sync", _a)
+cname("syncthing", _a)
+cname("www", _a)
