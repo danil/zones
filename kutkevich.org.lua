1ce130507334dafe2c62dbe2f2d46c7be4eec18f
Danil Kutkevich <danil@kutkevich.org>
2015-07-23 17:30:28 +0000

    kutkevich.org: add mx to lua

--- kutkevich.org.lua
+++ kutkevich.org.lua
@@ -11,6 +11,15 @@ a("tatyana", "188.134.8.88")
 a("yurij", "188.134.2.138")
 a(_a, "178.79.163.10")
 
+-- MX records
+mx(_a, "in1-smtp.messagingengine.com", 10)
+mx(_a, "in2-smtp.messagingengine.com", 20)
+mx(concat("g", _a), "alt1.aspmx.l.google.com", 5)
+mx(concat("g", _a), "alt2.aspmx.l.google.com", 5)
+mx(concat("g", _a), "aspmx.l.google.com", 1)
+mx(concat("g", _a), "aspmx2.googlemail.com", 10)
+mx(concat("g", _a), "aspmx3.googlemail.com", 10)
+
 -- CNAME records
 cname("danil", _a)
 cname("elpa", _a)
