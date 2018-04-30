7f078d6e092f6656b0972665819008f2d5f59b1b
Danil Kutkevich <danil@kutkevich.org>
2018-04-30 14:25:28 +0300

    kutkevich.ru: adding as separate zone

---
+++ kutkevich.ru.lua
@@ -0,0 +1,20 @@
+-- File: kutkevich.org.lua
+-- Zone: kutkevich.org
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "kutkevich.org"
+
+-- A records <http://www.luadns.com/help.html#a-record>
+-- @name  = relative name
+-- @ip    = IPv4 address
+-- @ttl   = TTL (default: user default TTL)
+-- a(name, ip, ttl)
+a(_a, "178.79.163.10")
+
+-- MX records <http://www.luadns.com/help.html#mx-record>
+-- @name      = relative name
+-- @exchanger = mail exchanger(fqdn)
+-- @prio      = priority (default: 0)
+-- @ttl       = TTL (default: user default TTL)
+-- mx(name, exchanger, prio, ttl)
+mx(_a, "mx.yandex.net", 10)
