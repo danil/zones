c9e594d8077e8013193c52e7deac4c1e890eaa21
Danil Kutkevich <danil@kutkevich.org>
2021-01-06 23:45:46 +0300

    feat: adding verification for the sprintomatic.com

--- kutkevich.org.lua
+++ kutkevich.org.lua
@@ -4,6 +4,9 @@
 -- Variable _a is replaced with zone name
 -- _a = "kutkevich.org"
 
+-- FIXME: remove me.
+txt("sprintomatic.com", "google-site-verification=wqaUIG_-yHs_J0WcSpbDriBXNWE5yqbbzsRZ58bNSj4")
+
 -- A records <http://www.luadns.com/help.html#a-record>
 -- @name  = relative name
 -- @ip    = IPv4 address
