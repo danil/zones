e6ded59836667ec6de7fbddc4df99a3b72694598
Danil Kutkevich <danil@kutkevich.org>
2021-01-26 15:32:02 +0300

    feat: replacing sprintomatic.com by nixmatic.com

--- kutkevich.org.lua
+++ kutkevich.org.lua
@@ -4,6 +4,9 @@
 -- Variable _a is replaced with zone name
 -- _a = "kutkevich.org"
 
+-- FIXME: remove me.
+txt("_github-challenge-nixmatic.nixmatic.com", "abb87f34e6")
+
 -- A records <http://www.luadns.com/help.html#a-record>
 -- @name  = relative name
 -- @ip    = IPv4 address
