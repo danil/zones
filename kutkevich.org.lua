2bbb8097ff6f8be818a489209e973ebb94a17ff0
Danil Kutkevich <danil@kutkevich.org>
2021-02-14 10:29:29 +0300

    Revert "minor"
    This reverts commit c33e63432ff9f7a8e6f743f6ee0444d772a8a441.

--- kutkevich.org.lua
+++ kutkevich.org.lua
@@ -4,11 +4,6 @@
 -- Variable _a is replaced with zone name
 -- _a = "kutkevich.org"
 
--- FIXME: remove me.
-
-txt("_github-challenge-nox64.nox64.com", "4c0c163464")
-txt(_a, "google-site-verification=4d_BmcZBOmQ1JXKfCGsUPaoLwtX9WW8sEH5qH9kTigY")
-
 -- A records <http://www.luadns.com/help.html#a-record>
 -- @name  = relative name
 -- @ip    = IPv4 address
